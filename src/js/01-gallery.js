507bc4f540887399835ef87526252fdb6899c064
Kravtsov <kravtsovofficial99@gmail.com>
2022-07-24 21:19:28 +0300

    work2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,42 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+// Change code below this line
 
 function createDiv(array) {
-    return array.reduce((acc, item) => acc + `<div class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
-</div>`, "")
+    return array.reduce((acc, item) => acc + `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`, "")
 }
 
 const div = document.querySelector(".gallery")
 div.insertAdjacentHTML("beforeend", createDiv(galleryItems) )
 
-div.addEventListener("click", divHendler)
 
-function divHendler(e) {
-    e.preventDefault()
-  const description = e.target.dataset.source;
-    const modal = basicLightbox.create(
-    `<img src = "${description}" width="800" height="600">`,
-    {
-      onShow: () => window.addEventListener(`keydown`, onPressKeyESC),
-      onClose: () => window.removeEventListener(`keydown`, onPressKeyESC)
-    }
-  )
-  modal.show();
-  
-  function onPressKeyESC(e) {
-    if (e.code === `Escape`) {
-      modal.close();
-    }
-  }
-}
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt"
+})
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
 
+// import throttle  from 'lodash.throttle';
+
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', function(time) {
+    console.log("time", time)
+});
