28e75e867c12071c4f4b2798266c1363b75ee063
Kravtsov <kravtsovofficial99@gmail.com>
2022-07-25 18:00:19 +0300

    work 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+import { throttle, times, update } from 'lodash';
 
+const form = document.querySelector(".feedback-form")
+const LOCALSTORAGE_KEY = "feedback-form-state"
+
+// form.addEventListener("input", throttle(savedAddInput, 500))
+// form.addEventListener("submit", submitForm)
+
+// updateInput()
+
+// function savedAddInput(event) {
+//     event.preventDefault()
+
+//     const {email, massage} = event.currentTarget
+// }
